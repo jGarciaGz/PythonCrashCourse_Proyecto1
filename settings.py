99bfe022903ffe176f80450a1ccd530167cdbb84
Jose Garcia <chepezgame@gmail.com>
2020-09-08 14:38:28 -0500

    Settings inicials listos settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings():
+    """Clase para guardar los ajustes para el juego """
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
+        
