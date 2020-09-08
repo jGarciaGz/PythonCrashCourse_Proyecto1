6c0d3e7733c6eb3c8704dcad48566970158381d6
Jose Garcia <chepezgame@gmail.com>
2020-09-08 14:30:45 -0500

    Ventana corriendo con color de fondo listo

---
+++ alien_invasion.py
@@ -0,0 +1,24 @@
+import sys
+
+import pygame
+
+def run_game():
+    #Inicio del juego y creacion de la pantalla del juego
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+    bg_color = (230, 230, 230)
+
+    #Inicio del ciclo del juego
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        screen.fill(bg_color)
+        
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    run_game()
